ecbda71f368311a5542453cfb8bb4f72337c30b4
Erdem Kaya <kerdem@live.com>
2024-09-17 17:00:36 +0200

    Tillgänglighetspassing - navbar design- finish

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ menuBtn.addEventListener('click', ()=> {
   const isExpanded = menuBtn.getAttribute('aria-expanded') === 'true'
   if(isExpanded) {
     menuBtn.setAttribute('aria-expanded', false)
+    mainMenu.addEventListener('animationend', () => {
+      mainMenu.classList.add('hide')
+    }, { once:true})
   } else {
+    mainMenu.classList.remove('hide')
     menuBtn.setAttribute('aria-expanded', true)
   }
 })
